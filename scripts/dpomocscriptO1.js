b3e40531f08eb8507b5a43f89cc4acee5d69e9f1
CreatorOfBiontium <johnsonpe83@gmail.com>
2023-04-19 19:35:17 +0200

    Position change

--- scripts/dpomocscriptO1.js
+++ scripts/dpomocscriptO1.js
@@ -33,6 +33,6 @@ function show_content_nd(){
 
 
 function hide_content_st(){
+    window.scrollTo({ top: 0, behavior: 'smooth' });    
     document.getElementById("stEll").innerHTML =  'Zda-li jste se dostali sem, tak jste již seznámeni s rozhraním webu. Vše zde ale probereme podrobněji. <br>Jestli chcete data <a href="https://creatorofbiontium.github.io/zasilky/dat1/poslizpravu.html">nahrát</a>, <a href="https://creatorofbiontium.github.io/zasilky/dat1/aktualizovani.html">upravit</a>, <a href="https://creatorofbiontium.github.io/zasilky/dat1/readfromdatabase.html">přečíst</a> nebo <a href="https://creatorofbiontium.github.io/zasilky/dat1/aktualizovani.html">smazat</a>, můžete vše udělat velmi jednoduše. <br><br><br><br>WEB <br><br>Na vrchní části obrazovky se nachází navigační panel, na kterém jsou odkazy na stránky zásilek.<br>Na stránkách pro správu dat jsou textová pole do kterých se vypisují potřebné informace. <br>...<br><br><br> <script src="scripts/dpomocscript.js"></script><button id="div4_butt_SprDat_Onclick-moreContent" class="div4_butt_all_Onclick-moreContent" onclick="show_content_st()">Více informací</button>'
-    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
